b74d0242fde5eaec5d2a68c08ed6450bfa944b59
Mark Gillespie <mark.gillespie81@gmail.com>
2021-02-25 20:58:31 -0500

    Add resizer bar

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,6 @@
 import * as THREE from "https://unpkg.com/three@0.125.1/build/three.module.js";
-import { Geoptic } from "./geoptic.js/build/geoptic.module.min.js";
-// import { Geoptic } from "./geoptic.js/src/geoptic.js";
+// import { Geoptic } from "./geoptic.js/build/geoptic.module.min.js";
+import { Geoptic } from "./geoptic.js/src/geoptic.js";
 
 import { bunny } from "./disk-bunny.js";
 
@@ -52,3 +52,70 @@ geoptic.doneLoading();
 
 // Start animating with geoptic
 geoptic.animate();
+
+// JS to manage resizing the editor pane. Stolen from
+// https://htmldom.dev/create-resizable-split-views/
+// Query the element
+const resizer = document.getElementById("resizer");
+const leftSide = resizer.previousElementSibling;
+const rightSide = resizer.nextElementSibling;
+
+// The current position of mouse
+let x = 0;
+let y = 0;
+
+// Width of right side
+// It's important that we manually resize the right side (geoptic)
+// and not the left side since geoptic doesn't resize nicely when
+// its parent does if the window doesn't change size
+let rightWidth = 0;
+
+// Handle the mousedown event
+// that's triggered when user drags the resizer
+const mouseDownHandler = function (e) {
+  // Get the current mouse position
+  x = e.clientX;
+  y = e.clientY;
+  rightWidth = rightSide.getBoundingClientRect().width;
+
+  // Attach the listeners to `document`
+  document.addEventListener("mousemove", mouseMoveHandler);
+  document.addEventListener("mouseup", mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+  // How far the mouse has been moved
+  const dx = e.clientX - x;
+  const dy = e.clientY - y;
+  console.log(e.clientX, dx);
+
+  const newRightWidth =
+    ((rightWidth - dx) * 100) /
+    resizer.parentNode.getBoundingClientRect().width;
+  rightSide.style.width = `${newRightWidth}%`;
+  document.body.style.cursor = "col-resize";
+  rightSide.style.userSelect = "none";
+  rightSide.style.pointerEvents = "none";
+
+  rightSide.style.userSelect = "none";
+  rightSide.style.pointerEvents = "none";
+  geoptic.onWindowResize();
+};
+
+const mouseUpHandler = function () {
+  resizer.style.removeProperty("cursor");
+  document.body.style.removeProperty("cursor");
+
+  rightSide.style.removeProperty("user-select");
+  rightSide.style.removeProperty("pointer-events");
+
+  rightSide.style.removeProperty("user-select");
+  rightSide.style.removeProperty("pointer-events");
+
+  // Remove the handlers of `mousemove` and `mouseup`
+  document.removeEventListener("mousemove", mouseMoveHandler);
+  document.removeEventListener("mouseup", mouseUpHandler);
+};
+
+// Attach the handler
+resizer.addEventListener("mousedown", mouseDownHandler);
